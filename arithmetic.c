8980dfbca9b4aa7bc392f6820f8508c4c8e80fd6
pranhsu-c <130463851+pranhsu-c@users.noreply.github.com>
2023-04-11 17:33:42 +0530

    Add files via upload

---
+++ arithmetic.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+main()
+{
+	int a=10, b=5, c, d, e, f, g, h;
+	
+	c=a+b;
+	 d=a*b;
+	  e=a/b;
+	   g=a-b;
+	    h=a%b;
+	
+	printf("addition of a & b=%d",c);
+	
+	printf("\nsubstraction of a & b=%d",g);
+	
+	printf("\nmultiplication of a & b=%d",d);
+	
+	printf("\ndivision of a & b=%d",e);
+	
+	printf("\nmodulation of a & b=%d",h);
+}
